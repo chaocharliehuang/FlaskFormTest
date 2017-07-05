84dc249b57f0931cbf902a86009926a76d2e660d
Chao Charlie Huang <chaocharliehuang@gmail.com>
2017-07-05 13:38:45 -0700

    added route with placeholder name

--- server.py
+++ server.py
@@ -6,6 +6,11 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/users/<username>')
+def show_user_profile(username):
+    print username
+    return render_template('success.html')
+
 @app.route('/users', methods=['POST'])
 def create_user():
     name = request.form['name']
