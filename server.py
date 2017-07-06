30bb5ce9fe84427ff8ac4f67ccbab339e3776b8f
Chao Charlie Huang <chaocharliehuang@gmail.com>
2017-07-05 21:03:21 -0700

    added session to store name and email info

--- server.py
+++ server.py
@@ -1,20 +1,24 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 
 app = Flask(__name__)
+app.secret_key = 'secret'
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/users/<username>')
+'''@app.route('/users/<username>')
 def show_user_profile(username):
-    return render_template('user.html', username = username)
+    return render_template('user.html', username = username)'''
 
 @app.route('/users', methods=['POST'])
 def create_user():
-    name = request.form['name']
-    email = request.form['email']
-    print name, email
-    return redirect('/')
+    session['name'] = request.form['name']
+    session['email'] = request.form['email']
+    return redirect('/show')
+
+@app.route('/show')
+def show_user():
+    return render_template('user.html')
 
 app.run(debug=True)
